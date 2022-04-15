bfc711221cbb3ef0a9d222b5f2904abade5400ee
SashBFa <bastienfajs@gmail.com>
2022-04-15 16:27:52 +0200

    initial folder empty

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { AnimatePresence } from "framer-motion";
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./assets/pages/About";
+import Discography from "./assets/pages/Discography";
+import Err from "./assets/pages/Err";
+import Gallery from "./assets/pages/Gallery";
+import Home from "./assets/pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <h1>tst</h1>
-    </div>
+    <BrowserRouter>
+      <AnimatePresence>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/discography" element={<Discography />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="*" element={<Err />} />
+        </Routes>
+      </AnimatePresence>
+    </BrowserRouter>
   );
 };
 
---
+++ src/assets/pages/Discography.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Discography = () => {
+  return (
+    <div>
+      <h1>Discography</h1>
+    </div>
+  );
+};
+
+export default Discography;
